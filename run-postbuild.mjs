65dc5ae19f34002bea9386cb61c940fac19046f6
CreamyTea13 <tylerkapp13@gmail.com>
2026-09-06 09:32:23 -0400

    Publish IndexNow ownership key

--- run-postbuild.mjs
+++ run-postbuild.mjs
@@ -13,3 +13,6 @@ try {
 } finally {
   fs.rmSync(runtimePath, { force: true });
 }
+
+const indexNowKey = "bb07bc1aac3124e31ea838541931a21b";
+fs.writeFileSync(`dist/${indexNowKey}.txt`, `${indexNowKey}\n`, "utf8");
